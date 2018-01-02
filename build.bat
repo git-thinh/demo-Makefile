14c7b7fb2e07213f41f17e651aa7dd939b3af53d
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-02 11:28:39 +0700

    update

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 
+:: Clean target
 del main.o app.exe
 
+:: Compile the source files into object files
 gcc -c main.cpp -o main.o
 
+:: Link the object files into a binary
 gcc -o app.exe main.o -lstdc++
