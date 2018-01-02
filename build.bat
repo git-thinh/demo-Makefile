1030c3baa3ebb8af557270aa16a07da1cc9ac33f
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-02 11:38:35 +0700

    update

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+@echo off
+test&cls
+echo.
 
 :: Clean target
 del main.o app.exe
@@ -7,3 +10,9 @@ gcc -c main.cpp -o main.o
 
 :: Link the object files into a binary
 gcc -o app.exe main.o -lstdc++
+
+call app.exe
+
+echo.
+echo.
+echo.
